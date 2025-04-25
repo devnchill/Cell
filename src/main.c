b204d9fea7f48b58b6b5d5515c8894e3775c7cd4
0xDev <169875752+devnchill@users.noreply.github.com>
2025-04-25 23:18:20 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -10,8 +10,15 @@ int main(int argc, char *argv[]) {
   char input[100];
   while (1) {
     printf("$ ");
+    //**********************************************
+    // extract input properly and remove new line character
     fgets(input, 100, stdin);
     input[strlen(input) - 1] = '\0';
+    //****************************************
+    // handle exit 0 command
+    if (strncmp(input, "exit 0", 6) == 0) {
+      return 0;
+    }
     printf("%s: command not found\n", input);
   }
   return 0;

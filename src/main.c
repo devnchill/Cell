f62444b96cf5f68316522a53f74f1b1586fe5bb3
0xDev <169875752+devnchill@users.noreply.github.com>
2025-04-25 23:33:38 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -2,6 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 
+int check_if_type_exists(char *input) {
+  input = input + 5;
+  if (strncmp(input, "echo", 4) == 0) {
+    printf("echo is a shell builtin\n");
+    return 0;
+  }
+  if (strncmp(input, "exit", 4) == 0) {
+    printf("type is a shell builtin\n");
+    return 0;
+  }
+  return 1;
+}
+
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
@@ -14,13 +27,17 @@ int main(int argc, char *argv[]) {
     // extract input properly and remove new line character
     fgets(input, 100, stdin);
     input[strlen(input) - 1] = '\0';
-    //************************** handle exit 0 command
+    //************************** handle exit 0 command ***************
     if (strcmp(input, "exit 0") == 0) {
       return 0;
     }
     //****************handle echo command****************************
     if (strncmp(input, "echo", 4) == 0) {
       printf("%s\n", input + 5);
+      continue;
+    }
+    //***************handle type command ***************************
+    if (check_if_type_exists(input) == 0) {
       continue;
     }
     printf("%s: command not found\n", input);

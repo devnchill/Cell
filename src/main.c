72733616de229c1ea65e59b98884299a1f7cff9d
0xDev <169875752+devnchill@users.noreply.github.com>
2025-04-28 21:46:22 +0530

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,21 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 void check_if_type_exists(char *input) {
+  int found = 0;
   input = input + 5;
-  if (strncmp(input, "echo", 4) == 0) {
-    printf("echo is a shell builtin\n");
-  } else if (strncmp(input, "exit", 4) == 0) {
-    printf("exit is a shell builtin\n");
-  } else if (strncmp(input, "type", 4) == 0) {
-    printf("type is a shell builtin\n");
-  } else {
+  // if (strncmp(input, "echo", 4) == 0) {
+  //   printf("echo is a shell builtin\n");
+  // } else if (strncmp(input, "exit", 4) == 0) {
+  //   printf("exit is a shell builtin\n");
+  // } else if (strncmp(input, "type", 4) == 0) {
+  //   printf("type is a shell builtin\n");
+  // } else {
+  //   printf("%s: not found\n", input);
+  // }
+  //***************Work with actual path***************
+  char *path_variable = getenv("PATH");
+  if (!path_variable) {
+    perror("Path Vairable Not Found");
+    return;
+  }
+  char *path_variable_copy = strdup(path_variable);
+  char *token = strtok(path_variable_copy, ":");
+  while (token != NULL) {
+    char *file_with_path = malloc(
+        (strlen(token) + strlen("/") + strlen(input) + 1) * sizeof(char));
+    file_with_path[0] = '\0';
+    strcat(file_with_path, token);
+    strcat(file_with_path, "/");
+    strcat(file_with_path, input);
+    if (access(file_with_path, F_OK) == 0) {
+      printf("%s is %s\n", input, file_with_path);
+      found = 1;
+      free(file_with_path);
+      break;
+    }
+    free(file_with_path);
+    token = strtok(NULL, ":");
+  }
+  free(path_variable_copy);
+  if (!found) {
     printf("%s: not found\n", input);
   }
 }
 
-int main(int argc, char *argv[]) {
+int main() {
   // Flush after every printf
   setbuf(stdout, NULL);
 

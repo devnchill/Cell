7ed5c3b7625897d78a4d79e9a6df857efa96ddcb
0xDev <169875752+devnchill@users.noreply.github.com>
2026-01-03 00:36:48 +0530

    feat: add REPL

--- src/main.c
+++ src/main.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
 #include <string.h>
 
+void get_command(char *command, size_t size) {
+  fgets(command, size, stdin);
+  command[strcspn(command, "\n")] = '\0';
+  return;
+}
+
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
   char command[1024];
-  printf("$ ");
 
-  fgets(command, sizeof(command), stdin);
-  command[strcspn(command, "\n")] = '\0';
+  while (1) {
 
-  printf("%s: command not found\n", command);
+    printf("$ ");
+
+    get_command(command, sizeof(command));
+
+    printf("%s: command not found\n", command);
+  }
 
   return 0;
 }

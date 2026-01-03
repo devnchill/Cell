b53273bd20723d566305137f5b058aa1751df055
0xDev <169875752+devnchill@users.noreply.github.com>
2026-01-03 14:30:43 +0530

    feat: exit builtin

--- src/main.c
+++ src/main.c
@@ -1,5 +1,11 @@
+#include "../include/hashmap.h"
+#include "../include/shell_builtin.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
+// TODO: will probably create a hashmap of type shell_builtin.command ->
+// shell_builtin;
 
 void get_command(char *command, size_t size) {
   fgets(command, size, stdin);
@@ -7,10 +13,21 @@ void get_command(char *command, size_t size) {
   return;
 }
 
+int builtin_exit(int argc, char **argv) { exit(0); }
+
+void add_builtins(void) {
+  shell_builtin e;
+  e.command = "exit";
+  e.info = "terminate the program with return code 0";
+  e.func = builtin_exit;
+  hashmap_add(e.command, e);
+}
+
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
+  add_builtins();
   char command[1024];
 
   while (1) {
@@ -18,6 +35,12 @@ int main(int argc, char *argv[]) {
     printf("$ ");
 
     get_command(command, sizeof(command));
+
+    shell_builtin *builtin = hashmap_get(command);
+    if (builtin != NULL) {
+      builtin->func(0, NULL);
+      continue;
+    }
 
     printf("%s: command not found\n", command);
   }

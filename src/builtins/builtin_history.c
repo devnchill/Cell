38d1843d851cc7a4f689660564560269910ebcaf
0xDev <169875752+devnchill@users.noreply.github.com>
2026-01-07 19:41:52 +0530

    feat: implement history n command

--- src/builtins/builtin_history.c
+++ src/builtins/builtin_history.c
@@ -1,11 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include <readline/history.h>
+
 int builtin_history(int argc, char **argv) {
-  typedef void *histdata_t;
-  HIST_ENTRY **history = history_list();
-  for (size_t i = 0; history[i]; i++) {
-    printf("    %zu  %s\n", i + 1, history[i]->line);
+  HISTORY_STATE *hs = history_get_history_state();
+  if (!hs || !hs->entries)
+    return 0;
+  int len = hs->length, n = len;
+
+  if (argc == 2) {
+    n = atoi(argv[1]);
+    if (n <= 0) {
+      fprintf(stderr, "history: invalid argument\n");
+      return -1;
+    }
+    if (n > len) {
+      fprintf(stdout, "history: showing all entries\n");
+      goto PRINT_ALL;
+    }
+  }
+
+  for (int i = len - n; i < len; i++) {
+    printf("    %d  %s\n", i + 1, hs->entries[i]->line);
+  }
+  return 0;
+
+PRINT_ALL:
+  for (int i = 0; i < len; i++) {
+    printf("    %d  %s\n", i + 1, hs->entries[i]->line);
   }
   return 0;
 }

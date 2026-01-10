cec30a8acfe956cf0b60fb8df0822e5df4126ab2
0xDev <169875752+devnchill@users.noreply.github.com>
2026-01-10 12:42:43 +0530

    feat: write history on exit

--- src/builtins/builtin_exit.c
+++ src/builtins/builtin_exit.c
@@ -1,2 +1,9 @@
+#include <stdio.h>
+
+#include <readline/history.h>
 #include <stdlib.h>
-int builtin_exit(int argc, char **argv) { exit(0); }
+int builtin_exit(int argc, char **argv) {
+  char *file = getenv("HISTFILE");
+  write_history(file);
+  exit(0);
+}

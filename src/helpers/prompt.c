a37e12b2c1204dad64fc708fc2b57be37e2dd9ae
darshan <darshanmparekh245624@gmail.com>
2026-03-03 15:12:55 +0530

    feat: display current working directly in prompt

---
+++ src/helpers/prompt.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+char *build_prompt(void) {
+    char *cwd = getcwd(NULL, 0);
+    if (!cwd)
+        return NULL;
+
+    size_t len = strlen(cwd) + 4;
+    char *prompt = malloc(len);
+    if (!prompt) {
+        free(cwd);
+        return NULL;
+    }
+
+    snprintf(prompt, len, "%s $ ", cwd);
+    free(cwd);
+    return prompt;
+}
